185f27913b23e6b7092643067660312163293427
debrian <debrianfigueroa@gmail.com>
2018-04-03 10:47:40 -0400

    Uploading all the files

---
+++ mainwindow.cpp
@@ -0,0 +1,396 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    repsEdit = ui->repsEdit;
+    workTimeEdit = ui->workTimeEdit;
+    restTimeEdit = ui->restTimeEdit;
+    prepTimeEdit = ui->prepTimeEdit;
+    totalTimeLabel = ui->totalTimeLabel;
+
+    totalTimeInSec = 0;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//------ Rep spinner ---------//
+
+void MainWindow::on_repsUpBtn_clicked()
+{
+    //Increment rep by 1 on label
+    try
+    {
+     //get current value
+     int repAmount = repsEdit->text().toInt();
+     //increment it
+     repAmount++;
+     //convert to string and display it
+     repsEdit->setText(QString::number(repAmount));
+
+    }catch(std::exception &e)
+    {
+        //value in label is not a number
+    }
+}
+
+void MainWindow::on_repsDownBtn_clicked()
+{
+    //Decrement rep by 1 on label
+    try
+    {
+     //get current value
+     int repAmount = repsEdit->text().toInt();
+     //decrement it only if it is greater than 0
+     if(repAmount > 0)
+     {
+         repAmount--;
+         //convert to string and display it
+         repsEdit->setText(QString::number(repAmount));
+     }
+
+    }catch(std::exception &e)
+    {
+        //value in label is not a number
+    }
+}
+
+//------------- Helper Methods ----------------//
+void updateTimeText(QLineEdit *timeEdit,int hours, int minutes, int seconds)
+{
+    QString time;
+    if(minutes < 10 && hours < 10 && seconds < 10)
+    {
+        time = QString("0%1:0%2:0%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(hours < 10 && minutes < 10)
+    {
+        time = QString("0%1:0%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }
+    else if(hours < 10 && seconds < 10)
+    {
+         time = QString("0%1:%2:0%3").arg(hours).arg(minutes).arg(seconds);
+         timeEdit->setText(time);
+    }else if(minutes < 10 && seconds < 10)
+    {
+        time = QString("%1:%2:0%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(minutes < 10)
+    {
+        time = QString("%1:0%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(seconds < 10)
+    {
+        time = QString("%1:%2:0%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(hours < 10)
+    {
+        time = QString("0%1:%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else
+    {
+        time = QString("%1:%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }
+}
+
+void updateTimeLabel(QLabel *timeEdit,int totalTime)
+{
+    int hours = totalTime / 3600;
+    int currentTimeInSec = (totalTime) - (hours * 3600);
+    int minutes = currentTimeInSec / 60;
+    int seconds = currentTimeInSec - (minutes * 60);
+
+    QString time;
+    if(minutes < 10 && hours < 10 && seconds < 10)
+    {
+        time = QString("0%1:0%2:0%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(hours < 10 && minutes < 10)
+    {
+        time = QString("0%1:0%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }
+    else if(hours < 10 && seconds < 10)
+    {
+         time = QString("0%1:%2:0%3").arg(hours).arg(minutes).arg(seconds);
+         timeEdit->setText(time);
+    }else if(minutes < 10 && seconds < 10)
+    {
+        time = QString("%1:%2:0%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(minutes < 10)
+    {
+        time = QString("%1:0%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(seconds < 10)
+    {
+        time = QString("%1:%2:0%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else if(hours < 10)
+    {
+        time = QString("0%1:%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }else
+    {
+        time = QString("%1:%2:%3").arg(hours).arg(minutes).arg(seconds);
+        timeEdit->setText(time);
+    }
+}
+
+int getHours(QStringList timeList)
+{
+    try
+    {
+        if(timeList.size() == 3)
+        {
+           return timeList.at(0).toInt();
+        }
+        //otherwise return 0 hours
+    }catch(std::exception &e)
+    {
+        //non number input
+    }
+
+    return 0;
+}
+
+int getMinutes(QStringList timeList)
+{
+    try
+    {
+        if(timeList.size() == 3)
+        {
+           return timeList.at(1).toInt();
+        }else if(timeList.size() == 2)
+        {
+            return timeList.at(0).toInt();
+        }
+        //otherwise return 0 minutes
+    }catch(std::exception &e)
+    {
+        //non number input
+    }
+    return 0;
+}
+
+int getSeconds(QStringList timeList)
+{
+    try
+    {
+        if(timeList.size() == 3)
+        {
+           return timeList.at(2).toInt();
+        }else if(timeList.size() == 2)
+        {
+            return timeList.at(1).toInt();
+        }else if(timeList.size() == 1)
+        {
+            return timeList.at(0).toInt();
+        }
+        //otherwise return 0 seconds
+    }catch(std::exception &e)
+    {
+        //non number input
+    }
+    return 0;
+}
+
+bool incrementTime(QLineEdit *timeEdit)
+{
+    //Increment time by 1 minute
+    try
+    {
+        QString workTimeText = timeEdit->text();
+        QStringList timeList = workTimeText.split(":");
+        int hours = getHours(timeList);
+        int minutes = getMinutes(timeList);
+        int seconds = getSeconds(timeList);
+        //increment time
+        minutes++;
+        //handle new hour reached
+        if(minutes > 59)
+        {
+            hours++;
+            minutes = 0;
+        }
+        //update time
+        updateTimeText(timeEdit,hours,minutes,seconds);
+        return true;
+    }catch(std::exception &e)
+    {
+        //non number input
+    }
+
+    return false;
+}
+
+bool decrementTime(QLineEdit *timeEdit)
+{
+    //Decrement time by 1 minute
+    try
+    {
+        QString workTimeText = timeEdit->text();
+        QStringList timeList = workTimeText.split(":");
+        int hours = getHours(timeList);
+        int minutes = getMinutes(timeList);
+        int seconds = getSeconds(timeList);
+        //decrement time
+        //handle new hour reached
+        if(minutes > 0)
+        {
+            minutes--;
+        }else if(minutes == 0 && hours > 0)
+        {
+            hours--;
+            minutes = 59;
+        }else
+        {
+            return false;
+        }
+        //update time
+       updateTimeText(timeEdit,hours,minutes,seconds);
+       return true;
+    }catch(std::exception &e)
+    {
+        //non number input
+    }
+
+    return false;
+}
+
+void updateTotalTime(int *timeInSec, QLineEdit *workTimeEdit, QLineEdit *restTimeEdit, QLineEdit *prepTimeEdit, QLabel *totalTimeLabel)
+{
+    //Get work time text
+    QString workTimeText = workTimeEdit->text();
+    QStringList timeList = workTimeText.split(":");
+    //transform text into time for each line edit
+    //WORK TIME
+    int workHour = getHours(timeList);
+    int workMinutes = getMinutes(timeList);
+    int workSec = getSeconds(timeList);
+    //REST TIME
+    QString restTimeText = restTimeEdit->text();
+    timeList = restTimeText.split(":");
+    int restHour = getHours(timeList);
+    int restMinutes = getMinutes(timeList);
+    int restSec = getSeconds(timeList);
+    //PREP TIME
+    QString prepTimeText = prepTimeEdit->text();
+    timeList = prepTimeText.split(":");
+    int prepHour = getHours(timeList);
+    int prepMinutes = getMinutes(timeList);
+    int prepSec = getSeconds(timeList);
+    //turn all the time into seconds
+    int hoursInSec = (workHour * 3600) + (restHour * 3600) + (prepHour * 3600);
+    int minutesInSec = (workMinutes * 60) + (restMinutes * 60) + (prepMinutes * 60);
+    //need to keep track of it for when the buttons try to increment or decrement the time
+    int total = workSec + restSec + prepSec + hoursInSec + minutesInSec;
+    *timeInSec = total;
+    //update time label
+    updateTimeLabel(totalTimeLabel,total);
+}
+
+//---------------- Spin Box Events -------------------//
+void MainWindow::on_workUpBtn_clicked()
+{
+    if(incrementTime(workTimeEdit))
+    {
+        totalTimeInSec += 60;
+        updateTimeLabel(totalTimeLabel,totalTimeInSec);
+    }
+}
+
+void MainWindow::on_workDownBtn_clicked()
+{
+   if(decrementTime(workTimeEdit))
+   {
+       if(totalTimeInSec >= 60)
+       {
+           totalTimeInSec -= 60;
+           updateTimeLabel(totalTimeLabel,totalTimeInSec);
+       }
+   }
+}
+
+void MainWindow::on_restUpBtn_clicked()
+{
+    if(incrementTime(restTimeEdit))
+    {
+        totalTimeInSec += 60;
+        updateTimeLabel(totalTimeLabel,totalTimeInSec);
+    }
+}
+
+void MainWindow::on_restDownBtn_clicked()
+{
+    if(decrementTime(restTimeEdit))
+    {
+        if(totalTimeInSec >= 60)
+        {
+            totalTimeInSec -= 60;
+            updateTimeLabel(totalTimeLabel,totalTimeInSec);
+        }
+    }
+}
+
+void MainWindow::on_prepUpBtn_clicked()
+{
+    if(incrementTime(prepTimeEdit))
+    {
+        totalTimeInSec += 60;
+        updateTimeLabel(totalTimeLabel,totalTimeInSec);
+    }
+}
+
+void MainWindow::on_prepDownBtn_clicked()
+{
+    if(decrementTime(prepTimeEdit))
+    {
+        if(totalTimeInSec >= 60)
+        {
+            totalTimeInSec -= 60;
+            updateTimeLabel(totalTimeLabel,totalTimeInSec);
+        }
+    }
+}
+
+//----------------- Line Edit input changes --------------------//
+void MainWindow::on_workTimeEdit_editingFinished()
+{
+    updateTotalTime(&totalTimeInSec,workTimeEdit,restTimeEdit,prepTimeEdit, totalTimeLabel);
+}
+
+void MainWindow::on_workTimeEdit_returnPressed()
+{
+    updateTotalTime(&totalTimeInSec,workTimeEdit,restTimeEdit,prepTimeEdit, totalTimeLabel);
+}
+
+void MainWindow::on_restTimeEdit_editingFinished()
+{
+    updateTotalTime(&totalTimeInSec,workTimeEdit,restTimeEdit,prepTimeEdit, totalTimeLabel);
+}
+
+void MainWindow::on_restTimeEdit_returnPressed()
+{
+    updateTotalTime(&totalTimeInSec,workTimeEdit,restTimeEdit,prepTimeEdit, totalTimeLabel);
+}
+
+void MainWindow::on_prepTimeEdit_editingFinished()
+{
+    updateTotalTime(&totalTimeInSec,workTimeEdit,restTimeEdit,prepTimeEdit, totalTimeLabel);
+}
+
+void MainWindow::on_prepTimeEdit_returnPressed()
+{
+    updateTotalTime(&totalTimeInSec,workTimeEdit,restTimeEdit,prepTimeEdit, totalTimeLabel);
+}
---
+++ mainwindow.h
@@ -0,0 +1,63 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QLabel>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_repsUpBtn_clicked();
+
+    void on_repsDownBtn_clicked();
+
+    void on_workUpBtn_clicked();
+
+    void on_workDownBtn_clicked();
+
+    void on_restUpBtn_clicked();
+
+    void on_restDownBtn_clicked();
+
+    void on_prepUpBtn_clicked();
+
+    void on_prepDownBtn_clicked();
+
+    void on_workTimeEdit_editingFinished();
+
+    void on_workTimeEdit_returnPressed();
+
+    void on_restTimeEdit_editingFinished();
+
+    void on_restTimeEdit_returnPressed();
+
+    void on_prepTimeEdit_editingFinished();
+
+    void on_prepTimeEdit_returnPressed();
+
+private:
+    Ui::MainWindow *ui;
+
+    //Widgets
+    QLineEdit *repsEdit;
+    QLineEdit *workTimeEdit;
+    QLineEdit *restTimeEdit;
+    QLineEdit *prepTimeEdit;
+    QLabel *totalTimeLabel;
+
+    //globals
+    int totalTimeInSec;
+};
+
+#endif // MAINWINDOW_H
